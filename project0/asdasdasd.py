8e1b5fc3e86e4c9a6bfe9cc2e69180d322c2e070
demetre3goisashvili <noobdemetre@gmail.com>
2024-02-26 09:23:35 -0800

    first commit

---
+++ project0/asdasdasd.py
@@ -0,0 +1,28 @@
+#gavaketot kalkulatori
+
+
+num1 = int(input("please enter your num1: "))
+choice = input("please make your choice: ")
+num2 = int(input("please enter your num2: "))
+
+if choice == "+":
+    print(num1 + num2)
+elif choice == "-":
+    print(num1 - num2)
+elif choice == "*":
+    print(num1 * num2)
+elif choice == "/":
+    print(num1 / num2)
+ 
+
+
+
+
+
+
+    
+
+
+    
+
+    
